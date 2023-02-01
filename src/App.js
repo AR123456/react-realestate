b925241ea660c83054a0c6c912aa12700b3e55d8
AR123456 <coloradoz99@gmail.com>
2023-02-01 05:59:03 -0700

    nav bar routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Explore from "./pages/Explore";
 import ForgotPassword from "./pages/ForgotPassword";
 import Offers from "./pages/Offers";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
-
-// import Navbar from "./Navbar";
 
 function App() {
   return (
@@ -20,8 +19,8 @@ function App() {
           <Route path="/sign-in" element={<SignIn />}></Route>
           <Route path="/sign-up" element={<SignUp />}></Route>
           <Route path="/forgot-password" element={<ForgotPassword />}></Route>
-          {/* <Navbar></Navbar> */}
         </Routes>
+        <Navbar></Navbar>
       </Router>
     </>
   );
