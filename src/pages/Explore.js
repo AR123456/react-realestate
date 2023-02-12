74c7c9860a62da1f185b8deddf09bdc3f239a4c4
AR123456 <coloradoz99@gmail.com>
2023-02-12 16:18:55 -0700

    pw email

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,36 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import rentCategoryImage from "../assets/jpg/rentCategoryImage.jpg";
-import sellCategoryImage from "../assets/jpg/sellCategoryImage.jpg";
 
 const Explore = () => {
   return (
-    <div className="explore">
-      <header>
-        <p className="pageHeader">Explor</p>
-      </header>
-      <main>
-        {/* slider */}
-        <p className="exploreCategoryHeading">Categories</p>
-      </main>
-      <div className="exploreCategories">
-        <Link to="/category/rent">
-          <img
-            src={rentCategoryImage}
-            alt="rent"
-            className="exploreCategoryImg"
-          />
-          <p className="exploreCategoryName">Places to rent</p>
-        </Link>
-        <Link to="/category/rent">
-          <img
-            src={sellCategoryImage}
-            alt="sell"
-            className="exploreCategoryImg"
-          />
-          <p className="exploreCategoryName">Places to buy</p>
-        </Link>
-      </div>
+    <div>
+      <h1>Explore</h1>
     </div>
   );
 };
