9062fa2f67ccf1caf9c73cd2b7fe397df9c7f8ad
AR123456 <coloradoz99@gmail.com>
2023-02-13 05:06:07 -0700

    explore page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import PrivateRoute from "./components/PrivateRoute";
 import Explore from "./pages/Explore";
 import ForgotPassword from "./pages/ForgotPassword";
 import Offers from "./pages/Offers";
+import Category from "./pages/Category";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
@@ -17,6 +18,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Explore />}></Route>
           <Route path="/offers" element={<Offers />}></Route>
+          <Route path="/category/:categoryName" element={<Category />}></Route>
           <Route path="/profile" element={<PrivateRoute />}>
             {/* this is the outlet user will be redircted to sign in if not looged in (the logic to check if signed in is in the provate route compoent) */}
             <Route path="/profile" element={<Profile />}></Route>
