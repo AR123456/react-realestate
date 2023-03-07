c9ffaa050d83c7b6d495f069b1930b5e97c1b72e
AR123456 <coloradoz99@gmail.com>
2023-03-07 06:28:50 -0700

    setting up server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,8 @@
-console.log("server...");
+//common js syntax ( not ES2015 import)
+const express = require("express");
+const dotenv = require("dotenv").config();
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
