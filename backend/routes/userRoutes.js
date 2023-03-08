57ab4f7bc15befc078006b1ae32fe4d9af7de1e9
AR123456 <coloradoz99@gmail.com>
2023-03-08 06:59:00 -0700

    routes and controller

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,14 +1,11 @@
 const express = require("express");
 
 const router = express.Router();
-// bring in controller logic
-const { registerUser, loginUser } = require("../controllers/userController");
+router.post("/", (req, res) => {
+  res.send("Register route");
+});
 
-// router.post("/", (req, res) => {
-//   res.send("Register route");
-// });
-// becomes
-router.post("/", registerUser);
-
-router.post("/login", loginUser);
+router.post("/login", (req, res) => {
+  res.send("Login route");
+});
 module.exports = router;
