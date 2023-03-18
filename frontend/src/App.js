4281b0dc6db039891a411ea00e0df8f649acffc5
AR123456 <coloradoz99@gmail.com>
2023-03-18 09:39:54 -0600

    react router dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <div>
-      My App
-      <Home></Home>
+      <Router>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home></Home>} />
+            <Route path="/login" element={<Login></Login>} />
+            <Route path="/register" element={<Register></Register>} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
