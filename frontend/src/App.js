161b76e4c9a7d5257220cc85fc37a39928ae9e4b
AR123456 <coloradoz99@gmail.com>
2023-03-18 09:40:45 -0600

    icons in header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import Header from "./components/Header";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Header from "./components/Header";
 
 function App() {
   return (
     <div>
       <Router>
         <div className="container">
-          <Header></Header>
+          <Header />
           <Routes>
             <Route path="/" element={<Home></Home>} />
             <Route path="/login" element={<Login></Login>} />
