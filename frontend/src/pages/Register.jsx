7ef24e994430e4e4c4e9b0603c6014079b753a6c
AR123456 <coloradoz99@gmail.com>
2023-03-19 09:04:34 -0600

    register form name value

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -9,12 +9,34 @@ const Register = () => {
     password2: "",
   });
   const { name, email, password, password2 } = formData;
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
   return (
     <div>
       <section className="heading">
         <h1>
           <FaUser>Register</FaUser>
         </h1>
+        <p>Create account</p>
+      </section>
+      <section className="form">
+        <form action="">
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              id="name"
+              name="name"
+              value={name}
+              onChange={onChange}
+              placeholder="Enter name"
+            />
+          </div>
+        </form>
       </section>
     </div>
   );
