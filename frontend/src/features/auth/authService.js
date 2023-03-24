027c1293647a9bcac471c7731096269ee427681c
AR123456 <coloradoz99@gmail.com>
2023-03-24 05:55:07 -0600

    auth service for registration

---
+++ frontend/src/features/auth/authService.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const API_URL = "/api/user";
+// register user
+const register = async (userData) => {
+  const response = await axios.post(API_URL, userData);
+  if (response.data) {
+    // save to local storage - it can only hold strings
+    localStorage.setItem("user", JSON.stringify(response.data));
+  }
+  // user data and token
+  return response.data;
+};
+
+const authService = {
+  register,
+};
+export default authService;
