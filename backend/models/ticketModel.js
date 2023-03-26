cc1e885b600ff7de2a560614a4f83fca81b5d95e
AR123456 <coloradoz99@gmail.com>
2023-03-26 08:51:10 -0600

    backend ticket model

---
+++ backend/models/ticketModel.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const ticketSchema = mongoose.Schema(
+  {
+    // relationship to ticket
+    user: {
+      //link to users object ID
+
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      // which collection - schema ?
+      ref: "User",
+    },
+    // what product is the ticket assocuated with
+    product: {
+      //
+      type: String,
+      required: [true, "Please select a product"],
+      // enumerated value array of possible products
+      enum: ["phone", "tablet", "printer", "monitor"],
+    },
+    description: {
+      type: String,
+      required: [true, "Please describe the problem"],
+    },
+
+    status: {
+      type: String,
+      required: true,
+      enum: ["new", "open", "closed"],
+      default: "new",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+module.exports = mongoose.model("Ticket", userSchema);
