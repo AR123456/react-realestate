2b8d6b97ccd3121742a5f1c8cb77878613321f7f
AR123456 <coloradoz99@gmail.com>
2023-03-27 07:20:04 -0600

    ticket controller

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -1,4 +1,27 @@
 const asyncHandler = require("express-async-handler");
-// need user and ticket models
-// user creating a ticket
-// get users tickets
+const User = require("../models/userModel");
+const Ticket = require("../models/TicketModel");
+
+//@desc Get users tickets
+//@route GET  /api/tickets/me
+//@access Private
+
+const getTickets = asyncHandler(async (req, res) => {
+  // use middleware for protected routes
+
+  res.status(200).json({ message: "getTickets" });
+});
+//@desc Create users tickets
+//@route POST  /api/tickets/me
+//@access Private
+
+const createTicket = asyncHandler(async (req, res) => {
+  // use middleware for protected routes
+
+  res.status(200).json({ message: "createTicket" });
+});
+
+module.exports = {
+  getTickets,
+  createTicket,
+};
--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-const {} = require("../controllers/ticketController");
+const { getTickets, createTicket } = require("../controllers/ticketController");
 
 const { protect } = require("../middleware/authMiddleware");
 
