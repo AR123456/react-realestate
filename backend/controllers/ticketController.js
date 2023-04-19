dced5b0ef65eb67372d0dd4947adb3b5103eaa37
AR123456 <coloradoz99@gmail.com>
2023-04-19 06:38:39 -0600

    get single ticket

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -16,6 +16,36 @@ const getTickets = asyncHandler(async (req, res) => {
   const tickets = await Ticket.find({ user: req.user.id });
   res.status(200).json(tickets);
 });
+
+//@desc Get a ticket
+//@route GET  /api/tickets/:id
+//@access Private
+
+const getTicket = asyncHandler(async (req, res) => {
+  // get user using the id and the JWT
+  const user = await User.findById(req.user.id);
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+  const ticket = await Ticket.findById(req.params.id);
+
+  if (!ticket) {
+    res.status(404);
+    throw new Error("Ticket not found");
+  }
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error("Not Authorized");
+  }
+  res.status(200).json(ticket);
+});
+module.exports = {
+  getTickets,
+  getTicket,
+  createTicket,
+};
+
 //@desc Create users tickets
 //@route POST  /api/tickets/me
 //@access Private
@@ -44,8 +74,3 @@ const createTicket = asyncHandler(async (req, res) => {
   // created
   res.status(201).json(ticket);
 });
-
-module.exports = {
-  getTickets,
-  createTicket,
-};
--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -2,12 +2,18 @@ const express = require("express");
 
 const router = express.Router();
 
-const { getTickets, createTicket } = require("../controllers/ticketController");
+const {
+  getTickets,
+  getTicket,
+  createTicket,
+} = require("../controllers/ticketController");
 
 const { protect } = require("../middleware/authMiddleware");
 
 // router.route is just a different syntax using it can chain to route
 router.route("/").get(protect, getTickets).post(protect, createTicket);
+
+router.route("/:id").get(protect, getTicket);
 
 //TODO what about admins working tickets
 
