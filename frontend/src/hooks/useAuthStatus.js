461000881777e3a1cc72e60ad9503c63e0daa9c6
AR123456 <coloradoz99@gmail.com>
2023-04-20 06:05:00 -0600

    use auth status hook

--- frontend/src/hooks/useAuthStatus.js
+++ frontend/src/hooks/useAuthStatus.js
@@ -4,4 +4,19 @@ import { useSelector } from "react-redux";
 export const useAuthStatus = () => {
   const [loggedIn, setLoggedIn] = useState(false);
   const [checkingStatus, setCheckingStatus] = useState(true);
+  // get user from state redux backend
+  const { user } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (user) {
+      setLoggedIn(true);
+    } else {
+      setLoggedIn(false);
+    }
+    setCheckingStatus(false);
+    //run when user changes
+  }, [user]);
+
+  return { loggedIn, checkingStatus };
 };
+// use this in the private route component
