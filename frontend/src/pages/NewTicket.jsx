74d785c67278b894fdb4acb6930feabc02dac4b6
AR123456 <coloradoz99@gmail.com>
2023-04-21 06:18:28 -0600

    setting up new ticket form

--- frontend/src/pages/NewTicket.jsx
+++ frontend/src/pages/NewTicket.jsx
@@ -1,6 +1,14 @@
-import React from "react";
-// need to re direct to login page if not logged in - useAuth status hook
+import { useState } from "react";
+// get user from global state using redux
+import { useSelector } from "react-redux";
+// need to re direct to login page if not logged in - useAuth status hook, nested route in app.js
 const NewTicket = () => {
+  const { user } = useSelector((state) => state.auth);
+  const [name, setName] = useState(user.name);
+  const [email, setEmail] = useState(user.email);
+  const [product, setProdict] = useState("");
+  const [description, setDescription] = useState("");
+
   return (
     <div>
       <h1>new</h1>
