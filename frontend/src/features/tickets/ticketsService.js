8a85b78535507d5b9239818e6c94eb15d90cb0d8
AR123456 <coloradoz99@gmail.com>
2023-04-24 06:51:21 -0600

    shell ticket service

--- frontend/src/features/tickets/ticketsService.js
+++ frontend/src/features/tickets/ticketsService.js
@@ -1,1 +1,12 @@
 // interact with API, hook up to form
+import axios from "axios";
+const API_URL = "/api/tickets/";
+
+// create ticket
+const createTicket = async () => {};
+
+const ticketService = {
+  createTicket,
+};
+
+export default ticketService;
