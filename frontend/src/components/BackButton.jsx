e59e252e70c5547c25a3c2f5ff20c98a5a1f4c35
AR123456 <coloradoz99@gmail.com>
2023-04-26 02:30:51 -0600

    back button component

---
+++ frontend/src/components/BackButton.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { FaArrowAltCircleLeft } from "react-icons/fa";
+import { Link } from "react-router-dom";
+const BackButton = ({ url }) => {
+  return (
+    <div>
+      BackButton
+      <h1>back</h1>
+    </div>
+  );
+};
+
+export default BackButton;
