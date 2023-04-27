bfbb2d1a0b2d7ecebb8142869c46eb43f8d52742
AR123456 <coloradoz99@gmail.com>
2023-04-27 05:11:10 -0600

    tickets page use effect

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,11 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Header from "./components/Header";
-import PrivateRoute from "./components/PrivateRoute";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import NewTicket from "./pages/NewTicket";
-import Tickets from "./pages/Tickets";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
@@ -23,9 +22,6 @@ function App() {
             {/* <Route path="/new-ticket" element={<NewTicket></NewTicket>} /> */}
             <Route path="/new-ticket" element={<PrivateRoute />}>
               <Route path="/new-ticket" element={<NewTicket />}></Route>
-            </Route>
-            <Route path="/tickets" element={<PrivateRoute />}>
-              <Route path="/tickets" element={<Tickets />}></Route>
             </Route>
           </Routes>
         </div>
