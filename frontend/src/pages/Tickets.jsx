b630d7bdad09b432b8dbbfc55dd147977a2854f4
AR123456 <coloradoz99@gmail.com>
2023-04-27 05:10:44 -0600

    update store

--- frontend/src/pages/Tickets.jsx
+++ frontend/src/pages/Tickets.jsx
@@ -4,6 +4,26 @@ import { getTickets, reset } from "../features/tickets/ticketsSlice";
 import Spinner from "../components/Spinner";
 import BackButton from "../components/BackButton";
 const Tickets = () => {
+  const { tickets, isLoading, isSuccess } = useSelector(
+    (state) => state.tickets
+  );
+  const dispatch = useDispatch();
+  // this useEffect will happen on unmount
+  useEffect(() => {
+    return () => {
+      if (isSuccess) {
+        dispatch(reset());
+      }
+    };
+  }, [dispatch, isSuccess]);
+
+  useEffect(() => {
+    dispatch(getTickets());
+  }, [dispatch]);
+
+  if (isLoading) {
+    return <Spinner />;
+  }
   return (
     <div>
       <h1>tickets</h1>
