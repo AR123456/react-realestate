2b3d28a6f3f981d0fa37ae0a0d644ecf13404d0a
AR123456 <coloradoz99@gmail.com>
2023-04-29 11:08:50 -0600

    setting up ticket page

--- frontend/src/pages/Ticket.jsx
+++ frontend/src/pages/Ticket.jsx
@@ -1,6 +1,20 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import { toast } from "react-toastify";
+import { useSelector, useDispatch } from "react-redux";
+import { getTicket, reset } from "../features/tickets/ticketsSlice";
+import { useParams } from "react-router-dom";
+import BackButton from "../components/BackButton";
+import Spinner from "../components/Spinner";
 const Ticket = () => {
+  const { ticket, isLoading, isSuccess, isError, message } = useSelector(
+    (state) => state.tickets
+  );
+  const params = useParams();
+  useEffect(() => {
+    if (isError) {
+      toast.error(message);
+    }
+  });
   return (
     <div>
       <h1>Details of a single ticket</h1>
