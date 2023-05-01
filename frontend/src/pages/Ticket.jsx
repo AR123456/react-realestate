87b5187228ee441a6618a4a2a59f5407ae2f12e8
AR123456 <coloradoz99@gmail.com>
2023-05-01 06:23:15 -0600

    getting close button on page

--- frontend/src/pages/Ticket.jsx
+++ frontend/src/pages/Ticket.jsx
@@ -1,8 +1,12 @@
 import React, { useEffect } from "react";
 import { toast } from "react-toastify";
 import { useSelector, useDispatch } from "react-redux";
-import { getTicket, reset } from "../features/tickets/ticketsSlice";
-import { useParams } from "react-router-dom";
+import {
+  getTicket,
+  reset,
+  closeTicket,
+} from "../features/tickets/ticketsSlice";
+import { useParams, useNavigate } from "react-router-dom";
 import BackButton from "../components/BackButton";
 import Spinner from "../components/Spinner";
 const Ticket = () => {
@@ -11,8 +15,10 @@ const Ticket = () => {
   );
   const params = useParams();
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   // get ticket Id from url
   const { ticketId } = useParams();
+
   useEffect(() => {
     if (isError) {
       toast.error(message);
@@ -20,7 +26,11 @@ const Ticket = () => {
     dispatch(getTicket(ticketId));
     //eslint-disable-next-line
   }, [isError, message, ticketId]);
-
+  const onTicketClose = () => {
+    dispatch(closeTicket(ticketId));
+    toast.success("Ticket Closed");
+    navigate("/tickets");
+  };
   if (isLoading) {
     return <Spinner></Spinner>;
   }
@@ -48,6 +58,11 @@ const Ticket = () => {
             <p>{ticket.description}</p>
           </div>
         </header>
+        {ticket.status !== "closed" && (
+          <button onClick={onTicketClose} className="btn btn-block btn-danger">
+            Close Ticket
+          </button>
+        )}
       </div>
     </div>
   );
