a05cb271b229ba98c328c2059c423481df307941
AR123456 <coloradoz99@gmail.com>
2023-05-04 06:23:52 -0600

    note item

--- frontend/src/components/NoteItem.jsx
+++ frontend/src/components/NoteItem.jsx
@@ -5,12 +5,25 @@ const NoteItem = ({ note }) => {
   return (
     <div>
       <h2>note</h2>
-      <div className="note" style={{
-        backgroundColor: note.isStaff ? "rgba(0,0,0,0.7" : "#fff",
-        color: note.isStaff ? "#fff" : "#000"
-    }}></div>
+      <div
+        className="note"
+        style={{
+          backgroundColor: note.isStaff ? "rgba(0,0,0,0.7" : "#fff",
+          color: note.isStaff ? "#fff" : "#000",
+        }}
+      >
+        <h4>
+          Note from{" "}
+          {note.isStaff ? <span>Staff</span> : <span>{user.name}</span>}
+        </h4>
+        <p>{note.text}</p>
+        <div className="note-date">
+          {new Date(note.createdAt).toLocaleDateString("en-US")}
+        </div>
+      </div>
     </div>
   );
 };
 
 export default NoteItem;
+// not seeing note in UI yes need to add create note model
